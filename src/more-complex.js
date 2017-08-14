540609592004251a378426b97bb6808d2b7a91ac
BasZhang <BasZhang@users.noreply.github.com>
2017-08-14 14:05:34 +0800

    Update more-complex.js

--- src/more-complex.js
+++ src/more-complex.js
@@ -4,6 +4,8 @@ if (os.name === 'windows') {
     phantom.outputEncoding = 'gbk';
     console.log("phantom.outputEncoding='gbk'");
 }
+phantom.clearCookies();
+
 var casper = require('casper').create({
     pageSettings: {
         javascriptEnabled: true,
@@ -56,7 +58,6 @@ var casper = require('casper').create({
     verbose: false
 });
 
-var url = "http://www.jsnt.lss.gov.cn:1002/query/";
 var account = casper.cli.args[0] || '';
 var pass = casper.cli.args[1] || '';
 
@@ -73,29 +74,29 @@ function(requestData, request) {
     }
 });
 
-casper.start(url);
+casper.start('http://www.jsnt.lss.gov.cn:1002/query/');
 
-var daga= {
-        method: 'post',
-        data: {
-            type: "1",
-            account: account,
-            password: pass
-        },
-        headers: {
-            Host: 'www.jsnt.lss.gov.cn:1002',
-            Connection: 'keep-alive',
-            Accept: '*/*',
-            Origin: 'http://www.jsnt.lss.gov.cn:1002',
-            'X-Requested-With': 'XMLHttpRequest',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            Referer: 'http://www.jsnt.lss.gov.cn:1002/query/index.html',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'zh-CN,zh;q=0.8'
-        }
+var daga = {
+    method: 'post',
+    data: {
+        type: "1",
+        account: account,
+        password: pass
+    },
+    headers: {
+        Host: 'www.jsnt.lss.gov.cn:1002',
+        Connection: 'keep-alive',
+        Accept: '*/*',
+        Origin: 'http://www.jsnt.lss.gov.cn:1002',
+        'X-Requested-With': 'XMLHttpRequest',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        Referer: 'http://www.jsnt.lss.gov.cn:1002/query/index.html',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.8'
+    }
 
-    };
+};
 casper.then(function() {
     var checkcode = this.evaluate(function() {
         return document.querySelector('input[name="checkcode"]').value;
@@ -109,13 +110,34 @@ casper.then(function() {
 });
 
 casper.thenOpen('http://www.jsnt.lss.gov.cn:1002/query/loginvalidate.html', daga);
+var daha = {
+    method: 'get',
+    headers: {
+        Host: 'www.jsnt.lss.gov.cn:1002',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36',
+        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        Referer: 'http://www.jsnt.lss.gov.cn:1002/query/left.html?roleId=',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate',
+        Connection: 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+    }
+
+};
 casper.then(function() {
+
     this.echo(this.getPageContent());
     var cookies = this.page.cookies;
     this.echo("cookie.length = " + cookies.length);
     for (var i in cookies) {
         this.echo(cookies[i].name + "=" + cookies[i].value);
+        phantom.addCookie(cookies[i]);
     }
+});
+casper.thenOpen('http://www.jsnt.lss.gov.cn:1002/query/person/personINFO_query.html', daha);
+casper.then(function() {
+    this.wait(1000);
+    this.capture('logon.png');
 });
 casper.run(function() {
     this.exit();
